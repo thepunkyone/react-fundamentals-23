5b09f5a838cc1788ef079ff9cf850cf718b0d2fe
Whitney Bluzma <thepunkyone@gmail.com>
2023-02-02 12:27:48 +0200

    feat: forms - uncontrolled, with validation

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,28 +4,43 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  const inputRef = React.useRef(null)
 
-  // 🐨 add the onSubmit handler to the <form> below
+  const [error, setError] = React.useState(null)
 
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  const handleChange = event => {
+    const {value} = event.target
+
+    const isValid = value === value.toLowerCase()
+
+    const errorMessage = isValid ? null : 'Username must be lower case'
+
+    setError(errorMessage)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    // const {value} = event.target.elements.username - alternative
+
+    onSubmitUsername(inputRef.current.value)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="username">Username:</label>
+        <input
+          ref={inputRef}
+          id="username"
+          type="text"
+          onChange={handleChange}
+        />
+        {error ? <div role="alert">{error}</div> : null}
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={!!error}>
+        Submit
+      </button>
     </form>
   )
 }
